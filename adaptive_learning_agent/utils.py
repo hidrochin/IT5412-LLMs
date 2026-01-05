f91dec7ced0a20decae3b576280a1ef82b0d0abe
hydrochin <hidro296@gmail.com>
2026-01-06 00:10:13 +0700

    fix: modified function

--- adaptive_learning_agent/utils.py
+++ adaptive_learning_agent/utils.py
@@ -7,31 +7,31 @@ import glob
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-# def pdf_to_markdown(pdf_path, output_dir):
-#     doc = pymupdf.open(pdf_path)
-#     md = pymupdf4llm.to_markdown(doc, header=False, footer=False, page_separators=True, ignore_images=True, write_images=False, image_path=None)
-#     md_cleaned = md.encode('utf-8', errors='surrogatepass').decode('utf-8', errors='ignore')
-#     output_path = Path(output_dir) / Path(doc.name).stem
-#     Path(output_path).with_suffix(".md").write_bytes(md_cleaned.encode('utf-8'))
-
 def pdf_to_markdown(pdf_path, output_dir):
-    pdf_path = Path(pdf_path)
-
     doc = pymupdf.open(pdf_path)
-    md = pymupdf4llm.to_markdown(
-        doc,
-        header=False,
-        footer=False,
-        page_separators=True,
-        ignore_images=True,
-        write_images=False,
-        image_path=None
-    )
+    md = pymupdf4llm.to_markdown(doc, header=False, footer=False, page_separators=True, ignore_images=True, write_images=False, image_path=None)
+    md_cleaned = md.encode('utf-8', errors='surrogatepass').decode('utf-8', errors='ignore')
+    output_path = Path(output_dir) / Path(doc.name).stem
+    Path(output_path).with_suffix(".md").write_bytes(md_cleaned.encode('utf-8'))
 
-    md_cleaned = md.encode("utf-8", errors="surrogatepass").decode("utf-8", errors="ignore")
+# def pdf_to_markdown(pdf_path, output_dir):
+#     pdf_path = Path(pdf_path)
 
-    output_path = (Path(output_dir) / pdf_path.stem).with_suffix(".md")
-    output_path.write_text(md_cleaned, encoding="utf-8")
+#     doc = pymupdf.open(pdf_path)
+#     md = pymupdf4llm.to_markdown(
+#         doc,
+#         header=False,
+#         footer=False,
+#         page_separators=True,
+#         ignore_images=True,
+#         write_images=False,
+#         image_path=None
+#     )
+
+#     md_cleaned = md.encode("utf-8", errors="surrogatepass").decode("utf-8", errors="ignore")
+
+#     output_path = (Path(output_dir) / pdf_path.stem).with_suffix(".md")
+#     output_path.write_text(md_cleaned, encoding="utf-8")
 
 
 def pdfs_to_markdowns(path_pattern, overwrite: bool = False):
